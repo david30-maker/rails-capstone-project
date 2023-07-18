e6969cd966574c65ad9b920f863530efd399b29d
david30-maker <davidokpah396@gmail.com>
2023-07-18 16:37:41 +0100

    ADD:implement view and controller item logic

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,2 +1,35 @@
 class ItemController < ApplicationController
+before_action :authenticate_user!
+
+def index 
+    @group = current_user.groups.find(params[:group_id])
+    @items = @group.items
+end
+
+def show
+    @group = current_user.groups.find(params[:group_id])
+    @item = @group.items.find(params[:id])
+end
+
+def new
+    @group = current_user.groups.find(params[:group_id])
+    @item = @group.items.new
+end
+
+def create
+    @group = current_user.groups.find(params[:group_id])
+    @item = @group.items.new(item_params)
+    if @item.save
+        redirect_to group_item_path(@group), notice: 'Item was successfully created.'
+    else
+        render :new
+    end
+end
+
+private
+
+def item_params
+    params.require(:item).permit(:name, :amount, :author_id)
+end
+
 end
