d874b16df0dbd9f9e5abacef4562a25b690a43c2
david30-maker <davidokpah396@gmail.com>
2023-07-18 02:08:47 +0100

    update the code be dry

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,22 +1,11 @@
 class ConfirmationsController < Devise::ConfirmationsController
-    # GET /resource/confirmation/new
-    def new
-      super
-    end
-  
-    # POST /resource/confirmation
-    def create
-      super
-    end
-  
-    # GET /resource/confirmation?confirmation_token=abcdef
-    def show
-      super
-    end
+  # GET /resource/confirmation/new
 
+  # POST /resource/confirmation
 
-    def set_current_user
-        @current_user = current_user
-        end
+  # GET /resource/confirmation?confirmation_token=abcdef
+
+  def set_current_user
+    @current_user = current_user
   end
-  
+end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,24 +1,23 @@
 class HomeController < ApplicationController
+  def index; end
 
-    def index; end
+  # def index
+  #     @expense = Expense.new
+  #     @expenses = Expense.all
+  # end
 
-    # def index
-    #     @expense = Expense.new
-    #     @expenses = Expense.all
-    # end
-    
-    # def create
-    #     @expense = Expense.new(expense_params)
-    #     if @expense.save
-    #     redirect_to root_path, notice: "Expense added successfully"
-    #     else
-    #     render :index
-    #     end
-    # end
-    
-    # private
-    
-    # def expense_params
-    #     params.require(:expense).permit(:title, :amount, :date)
-    # end
+  # def create
+  #     @expense = Expense.new(expense_params)
+  #     if @expense.save
+  #     redirect_to root_path, notice: "Expense added successfully"
+  #     else
+  #     render :index
+  #     end
+  # end
+
+  # private
+
+  # def expense_params
+  #     params.require(:expense).permit(:title, :amount, :date)
+  # end
 end
