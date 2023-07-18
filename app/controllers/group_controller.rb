b4534cafbbd01c297c9357e1529b9aa1fe74a069
david30-maker <davidokpah396@gmail.com>
2023-07-18 16:03:50 +0100

    ADD:implement group view functionality

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -1,2 +1,32 @@
 class GroupController < ApplicationController
+    before_action :require_login
+
+    def index
+        @groups = current_user.groups
+    end
+
+    def show
+        @group = cuurent_user.groups.find(params[:id])
+        @total_amount = @group.items.sum(:amount)
+    end
+
+    def new
+        @group = current_user.groups.new
+    end
+
+    def create
+        @group = current_user.groups.new(group_params)
+        if @group.save
+            redirect_to group_path(@group), notice: "Group created successfully"
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def group_params
+        params.require(:group).permit(:name, :icon)
+    end
+
 end
